2da84d13af20503e8e334008e73b3ca1bafbaa84
lp-research <info@lp-research.com>
2014-01-20 01:37:48 +0100

    Removed gen and lib dirs in Android application.

--- LpmsBNativeAndroidLibrary/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/nikolaipetersen/Dropbox/klaus/lpmssoftware/openmat/LpmsBNativeAndroidLibrary/bin/AndroidManifest.xml : \
-/Users/nikolaipetersen/Dropbox/klaus/lpmssoftware/openmat/LpmsBNativeAndroidLibrary/AndroidManifest.xml \
--- LpmsBNativeAndroidLibrary/bin/LpmsBNativeAndroidLibrary-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/Users/nikolaipetersen/Dropbox/klaus/lpmssoftware/openmat/LpmsBNativeAndroidLibrary/bin/LpmsBNativeAndroidLibrary-debug-unaligned.apk : \
-/Users/nikolaipetersen/Dropbox/klaus/lpmssoftware/openmat/LpmsBNativeAndroidLibrary/bin/LpmsBNativeAndroidLibrary.ap_ \
-/Users/nikolaipetersen/Dropbox/klaus/lpmssoftware/openmat/LpmsBNativeAndroidLibrary/bin/classes.dex \
-/Users/nikolaipetersen/Dropbox/klaus/lpmssoftware/openmat/LpmsBNativeAndroidLibrary/libs/GraphView-3.1.1.jar \
-/Users/nikolaipetersen/Dropbox/klaus/lpmssoftware/openmat/LpmsBNativeAndroidLibrary/libs/android-support-v4.jar \

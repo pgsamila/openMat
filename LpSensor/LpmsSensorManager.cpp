d49a2638f2ca5ab8041a80cceece4f686b78d28a
Klaus <klaus@lp-research.com>
2014-03-14 17:11:00 +0900

    Changed notation of order of quaternion values in csv file writer. Was qx, qy, qz, qw. CORRECT: qw, qx, qy, qz.

--- LpSensor/LpmsSensorManager.cpp
+++ LpSensor/LpmsSensorManager.cpp
@@ -307,7 +307,7 @@ bool LpmsSensorManager::saveSensorData(const char* fn)
 	saveDataHandle.open(fn, ios_base::out);
 	saveDataHandle.rdbuf()->pubsetbuf(writeBuffer, 65536);
 	if (saveDataHandle.is_open() == true) {	
-		saveDataHandle << "SensorId, TimeStamp (s), FrameNumber, AccX (g), AccY (g), AccZ (g), GyroX (deg/s), GyroY (deg/s), GyroZ (deg/s), MagX (uT), MagY (uT), MagZ (uT), EulerX (deg), EulerY (deg), EulerZ (deg), QuatX, QuatY, QuatZ, QuatW, LinAccX (m/s^2), LinAccY (m/s^2), LinAccZ (m/s^2), Pressure (hPa), Altitude (m), Temperature (degC), HeaveMotion (m)\n";
+		saveDataHandle << "SensorId, TimeStamp (s), FrameNumber, AccX (g), AccY (g), AccZ (g), GyroX (deg/s), GyroY (deg/s), GyroZ (deg/s), MagX (uT), MagY (uT), MagZ (uT), EulerX (deg), EulerY (deg), EulerZ (deg), QuatW, QuatX, QuatY, QuatZ, LinAccX (m/s^2), LinAccY (m/s^2), LinAccZ (m/s^2), Pressure (hPa), Altitude (m), Temperature (degC), HeaveMotion (m)\n";
 
 		cout << "[LpmsSensorManager] Writing LPMS data to " << fn << endl;	
 		

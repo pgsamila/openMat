d94ea4ac67e751b157fb97995b202d42f9a03856
Klaus <klaus@lp-research.com>
2014-04-03 15:24:10 +0900

    Added C3D file parser to LpMocap

---
+++ LpMocap/C3dParser.h
@@ -0,0 +1,25 @@
+#ifndef C3D_PARSER
+#define C3D_PARSER
+
+#include <iostream>
+#include <fstream>
+
+#include <boost/cstdint.hpp>
+
+class C3dParser {
+public:
+	std::ifstream input_file_stream;	
+	int no_markers_;
+	int no_frames_;
+
+public:
+	~C3dParser(void);
+	void OpenFile(const char* fn);
+	void ReadHeader(void);
+	void ReadParameterSections(void);
+	bool ReadData(double *data_x, double *data_y, double *data_z);
+	int GetNoFrames(void);
+	int GetNoMarkers(void);
+};
+
+#endif

7d0d30c709c80596e94a5b72921df8e91394aa66
Klaus <klaus@lp-research.com>
2014-05-25 21:05:52 +0200

    Adjusted OS version to latest changes in CS firmware.

--- LpmsFirmware/LpmsSource/HeaveMotionOS.c
+++ LpmsFirmware/LpmsSource/HeaveMotionOS.c
@@ -3,6 +3,10 @@
 
 #include "HeaveMotion.h"
 
+extern LpVector3f heaveOutput;
+extern LpVector4f q;
+extern LpVector3f a;
+
 float lpFilter(float v, float *c, float a)
 {
 	return 0.0f;
@@ -17,6 +21,12 @@ void initHeaveMotion(void)
 {
 }
 
+/*	INPUT
+	a: Accelerometer data
+	q: Orientation quaternion
+
+	OUTPUT
+	heaveOutput.data[2]: Z-axis translation */
 void calculateHeaveMotion(float T)
 {
 }
--- LpmsFirmware/LpmsSource/LpFilterCVersionOS.c
+++ LpmsFirmware/LpmsSource/LpFilterCVersionOS.c
@@ -10,6 +10,14 @@ void lpFilterInit(void)
 {
 }
 
+/* 	INPUT
+	a: Calib. accelerometer data (a.data[0] = x-axis, a.data[1] = y-axis etc.)
+	b: Calib. magnetometer data
+	g: Calib. gyroscope data
+	T: Timestamp (Time difference between frames in ms)
+	
+	OUTPUT:
+	qOut: Orientation quaternion (q.data[0] = w-axis, q.data[1] = x-axis etc.) */
 void lpFilterUpdate(LpVector3f a, LpVector3f b, LpVector3f g, LpVector4f *qOut,
 	float T, float bInclination, float *bNoise)
 {	
@@ -22,4 +30,16 @@ void lpOrientationFromAccMag(LpVector3f b, LpVector3f a, LpVector3f *r, float *i
 void lpFilterEulerUpdate(LpVector3f a, LpVector3f b, LpVector3f g, LpVector3f *rOut, LpVector4f *qOut,
 	float T, float bInclination, float *bNoise)
 {	
+}
+
+void gyrOnlineCal(float T)
+{
+}
+
+void gyroToInertial(LpVector4f qI, LpVector3f *wO)
+{
+}
+
+void gyroToInertialEuler(void)
+{
 }

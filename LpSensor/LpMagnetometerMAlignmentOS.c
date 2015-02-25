0cfdf396219a4e3b466f4810e100cf65b29001d2
Birdy YAP <bediyap@gmail.com>
2015-02-25 13:58:07 +0900

    add missing functions

--- LpSensor/LpMagnetometerMAlignmentOS.c
+++ LpSensor/LpMagnetometerMAlignmentOS.c
@@ -4,7 +4,12 @@
 ** Contact: LP-Research (info@lp-research.com)
 ***********************************************************************/
 
-#include "LpMagnetometerCalibration.h"
+#include "LpMagnetometerMAlignment.h"
+
+float bMACalPythag(float a, float b)
+{
+    return 0.0f;
+}
 
 void bMACalInitEllipsoidFit(void)
 {
@@ -14,7 +19,12 @@ void bMAUpdateMap(LpVector4f q, LpVector3f b, LpVector3f bR)
 {
 }
 
+int bMACalCalcSVD(float **mat, int m, int n, float **w, float **v, int maxCalElements)
+{
+    return 1;
+}
+
 int bMACalFitEllipsoid(LpMatrix3x3f *R, LpVector3f *t)
 {	
-	return 1;
+    return 1;
 }
